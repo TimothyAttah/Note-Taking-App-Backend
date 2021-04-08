fe6c05f49a582d2e23b8af9cb3b36e2813dd10d0
Timothy Attah <timothyattah733@gmail.com>
2021-04-08 14:51:01 +0100

    remove some text in server.js

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const app = express()
 app.use( express.json() )
 app.use( cors() )
 
-app.get( '/', ( req, res ) => {
-  res.send('This is a note taking app')
-} )
+// app.get( '/', ( req, res ) => {
+//   res.send('This is a note taking app')
+// } )
 
 if ( process.env.NODE_ENV === 'production' ) {
   app.use( express.static( 'client/build' ) )
