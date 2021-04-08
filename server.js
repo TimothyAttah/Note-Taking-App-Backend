c534f650e539244844a3167e26d2836083bb020a
Timothy Attah <timothyattah733@gmail.com>
2021-04-08 04:59:21 +0100

    Backend setup with express

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require( 'express' );
+const cors = require( 'cors' );
+const path = require('path')
+
+const app = express()
+
+app.use( express.json() )
+app.use( cors() )
+
+app.get( '/', ( req, res ) => {
+  res.send('This is Expense tracker app')
+} )
+
+if ( process.env.NODE_ENV === 'production' ) {
+  app.use( express.static( 'client/build' ) )
+  
+  app.get( '*', ( req, res ) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
+console.log(`Current directory is: ${__dirname}`)
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=> console.log(`Server is running on port: ${PORT}`))
