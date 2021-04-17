1793a68f48bc599db3da7ee97beac69547f318bc
Timothy Attah <timothyattah733@gmail.com>
2021-04-17 18:15:22 +0100

    Created user unfollow route

--- controllers/userController.js
+++ controllers/userController.js
@@ -37,3 +37,20 @@ module.exports.followUsers = async ( req, res ) => {
      return res.status( 500 ).json( { error: error.message } );
   }
 }
+
+module.exports.unFollowUsers = async ( req, res ) => {
+  try {
+    await User.findByIdAndUpdate( req.body.unfollowId, {
+      $pull:{followers:req.user._id}
+    }, { new: true }, ( async ( err, result ) => {
+      if ( err )
+        return res.status( 404 ).json( { error: err.message } )
+      await User.findByIdAndUpdate( req.user._id, {
+       $push: {following: req.body.unfollowId}
+      }, { new: true } )
+      res.status(200).json({message: 'You are unfollowing', result})
+    }))
+  } catch (error) {
+     return res.status( 500 ).json( { error: error.message } );
+  }
+}
