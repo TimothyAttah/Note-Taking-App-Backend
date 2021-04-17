77af1076e751391986127c6ebf3e7cd515ac5b31
Timothy Attah <timothyattah733@gmail.com>
2021-04-17 18:09:13 +0100

    Created user following route

--- controllers/userController.js
+++ controllers/userController.js
@@ -20,3 +20,20 @@ module.exports.getUser = async ( req, res ) => {
     return res.status( 500 ).json( { error: error.message } );
   }
 }
+
+module.exports.followUsers = async ( req, res ) => {
+  try {
+    await User.findByIdAndUpdate( req.body.followId, {
+      $push:{followers:req.user._id}
+    }, { new: true }, ( async ( err, result ) => {
+      if ( err )
+        return res.status( 404 ).json( { error: err.message } )
+      await User.findByIdAndUpdate( req.user._id, {
+       $push: {following: req.body.followId}
+      }, { new: true } )
+      res.status(200).json({message: 'You are following', result})
+    }))
+  } catch (error) {
+     return res.status( 500 ).json( { error: error.message } );
+  }
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,9 +2,11 @@ const express = require( 'express' );
 const router = express.Router();
 const auth = require( '../middlewares/auth' );
 const {
-  getUser
+  getUser, followUsers
 } = require( '../controllers/userController' );
 
 router.get( '/:id', auth, getUser );
+
+router.patch( '/follow', auth, followUsers );
 
 module.exports = router;
