4def71f1f25f6c896f3e0433af5db620479166ee
Timothy Attah <timothyattah733@gmail.com>
2021-04-17 19:25:09 +0100

    Change some files in followUser route in userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,20 +23,40 @@ module.exports.getUser = async ( req, res ) => {
 
 module.exports.followUsers = async ( req, res ) => {
   try {
-    await User.findByIdAndUpdate( req.body.followId, {
-      $push:{followers:req.user._id}
-    }, { new: true }, ( async ( err, result ) => {
-      if ( err )
-        return res.status( 404 ).json( { error: err.message } )
-      await User.findByIdAndUpdate( req.user._id, {
-       $push: {following: req.body.followId}
-      }, { new: true } )
-      res.status(200).json({message: 'You are following', result})
-    }))
+    User.findByIdAndUpdate( req.body.followId, {
+     $push: {followers: req.user._id}
+    }, { new: true }, ( err, result ) => {
+      if ( err ) {
+        return res.status( 404 ).json( { error: err.message } );
+      }
+      User.findByIdAndUpdate( req.user._id, {
+        $push: {following: req.body.followId}
+      }, { new: true } ).select( '-password' ).then( result => {
+          res.status(200).json({message: 'You are following', result})
+      })  
+   })
   } catch (error) {
      return res.status( 500 ).json( { error: error.message } );
   }
 }
+// module.exports.followUsers = async ( req, res ) => {
+//   try {
+//     await User.findByIdAndUpdate( req.body.followId, {
+//       $push:{followers:req.user._id}
+//     }, { new: true }, ( async ( err, result ) => {
+//       if ( err )
+//         return res.status( 404 ).json( { error: err.message } )
+//      await User.findByIdAndUpdate( req.user._id, {
+//        $push: {following: req.body.followId}
+//      }, { new: true }, async ( myFollowers ) => {
+       
+//        res.status(200).json({message: 'You are following', myFollowers})
+//       } )
+//     }))
+//   } catch (error) {
+//      return res.status( 500 ).json( { error: error.message } );
+//   }
+// }
 
 module.exports.unFollowUsers = async ( req, res ) => {
   try {
