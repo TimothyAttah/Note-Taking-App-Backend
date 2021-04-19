551005e68d7fe03f61fbe882d59b9af6741d0cf4
Timothy Attah <timothyattah733@gmail.com>
2021-04-19 18:54:44 +0100

    added new api

--- server.js
+++ server.js
@@ -18,23 +18,24 @@ app.use( express.json() );
 app.use( cors() );
 
 
-app.use( '/api/user', require( './routes/authRoutes' ) );
-app.use( '/api/notes', require( './routes/notesRoutes' ) );
-app.use( '/api/auth/user', require( './routes/userRoutes' ) );
+app.use( 'https://notes-taking-app-front-end.herokuapp.com/api/user', require( './routes/authRoutes' ) );
+app.use( 'https://notes-taking-app-front-end.herokuapp.com/api/notes', require( './routes/notesRoutes' ) );
+app.use( 'https://notes-taking-app-front-end.herokuapp.com/api/auth/user', require( './routes/userRoutes' ) );
+
+//  app.get( '/', ( req, res ) => {
+//     res.send('Hello world')
+//   })
+  
 
 if ( process.env.NODE_ENV === 'production' ) {
   app.use( express.static( 'client/build' ) )
 
-  app.get( '/', ( req, res ) => {
-    res.send('Hello world')
-  })
-  
   // app.get( '*', ( req, res ) => {
   //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   // })
 
   app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    res.sendFile(path.join(__dirname, '/client/build/index.html', 'index.html'));
 });
 
 }
