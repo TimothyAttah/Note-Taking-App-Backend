6a3b8794a8d14d1525c0fa1845669033958b49ac
Timothy Attah <timothyattah733@gmail.com>
2021-04-19 01:53:53 +0100

    Added new file path in server.js

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const app = express();
 const mongoDB = require( './config/db' );
 mongoDB();
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.use( express.json() );
 app.use( cors() );
 
@@ -23,9 +25,14 @@ app.use( '/api/auth/user', require( './routes/userRoutes' ) );
 if ( process.env.NODE_ENV === 'production' ) {
   app.use( express.static( 'client/build' ) )
   
-  app.get( '*', ( req, res ) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
+  // app.get( '*', ( req, res ) => {
+  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  // })
+
+  app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
 }
 
 
